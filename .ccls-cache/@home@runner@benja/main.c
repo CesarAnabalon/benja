8f857694d68c39367c7f870c149b8fecb690a217
CesarAnabalon <128104619+CesarAnabalon@users.noreply.github.com>
2023-09-02 20:34:35 +0000

    se creo funcion

--- .ccls-cache/@home@runner@benja/main.c
+++ .ccls-cache/@home@runner@benja/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void funcion ()
+{
+
+  for(int i = 0; i < 10; i++)
+    {
+      printf("%i",i);
+    }
+}
+
 int main(void) {
   printf("Hello World\n");
+  funcion();
   return 0;
 }
